a3deff15d3ce873856764390163f6b9711f344f1
Fish4203 <26777965+Fish4203@users.noreply.github.com>
2021-07-16 18:47:25 +1000

    changed orbitals and added atmospheres and changed planet gen

--- mysite/solapi/generation.py
+++ mysite/solapi/generation.py
@@ -1,6 +1,15 @@
 from models import *
 import ramdom
 
+
+# v\left(l_{1}\sin\left(\frac{t_{1}^{2}}{a_{1}t_{2}}\right)+x_{1},h_{1}\cos\left(\frac{t_{1}^{2}}{a_{1}t_{2}}\right)+x_{2}\right)
+
+# orbit shit
+# \left(l_{1}\sin\left(\frac{\pi\cdot t_{1}^{2}}{a_{1}t_{1}a}\right)+x_{1},h_{1}\cos\left(\frac{\pi\cdot t_{1}^{2}}{a_{1}t_{1}a}\right)+x_{2}\right)
+# x = L * Sin(pi * t^2 / p * t * a) + x
+# y = H * Cos(pi * t^2 / p * t * a) + y
+
+
 def systemGen(name, seed):
     system = System(name=name, seed=name)
 
@@ -11,16 +20,19 @@ def systemGen(name, seed):
     if tempVal = 1:
         # unery system with one star
 
-        system.stars.add(starGen())
+        system.stars.add(starGen(name= name+'a', str(random.random())))
 
         for i in range(0, random.randint(10)):
             # how many planets are going to be gened
             system.planets.add(planetGen())
+
+
     elif tempVal = 2:
         # binery system with 2 star
 
         system.stars.add(starGen())
         system.stars.add(starGen())
+        # todo: orbit shit
 
         for i in range(0, random.randint(10)):
             # how many planets are going to be gened
@@ -28,119 +40,80 @@ def systemGen(name, seed):
 
 
 
-def generate(self, name, seed):
-    self.name = name
-    self.seed = seed
-    random.seed = seed
+def starGen(name, seed):
+    # this should be mostly done and working for unery systems
+    star = Star(name=name, seed=seed)
+    random.seed = star.seed
 
     t_val = random.random()  # Temp variable
 
     if t_val <= 0.0000003:
-        self.starClass = 'O'
-        self.mass = random.randrange(160000, 320000)  # *10^26 kilograms
-        self.radius = self.mass * 0.4125  # In Solar radii (6.975*10^5km)
-        self.temperature = math.floor(4*math.pi*self.radius*self.radius * 42.6264386916 + 6666.67)  # Kelvin (many thousands) The area of the sun multiplies by the gradient of the relationship between area and termperate for its class
+        star.starClass = 'O'
+        star.mass = random.randrange(160000, 320000)  # *10^26 kilograms
+        star.radius = star.mass * 0.4125  # In Solar radii (6.975*10^5km)
+        star.temperature = math.floor(4*math.pi*star.radius*star.radius * 42.6264386916 + 6666.67)  # Kelvin (many thousands) The area of the sun multiplies by the gradient of the relationship between area and termperate for its class
     elif 0.0000003 < t_val <= 0.0013003:
-        self.starClass = 'B'
-        self.mass = random.randrange(21000, 160000)  # *10^26 kilograms
-        self.radius = self.mass * 0.345323741007 + 1.47842  # In Solar radii (6.975*10^5km)
-        self.temperature = math.floor(4*math.pi*self.radius*self.radius * 39.4729521557 + 8392.86)
+        star.starClass = 'B'
+        star.mass = random.randrange(21000, 160000)  # *10^26 kilograms
+        star.radius = star.mass * 0.345323741007 + 1.47842  # In Solar radii (6.975*10^5km)
+        star.temperature = math.floor(4*math.pi*star.radius*star.radius * 39.4729521557 + 8392.86)
     elif 0.0013003 < t_val <= 0.0073003:
-        self.starClass = 'A'
-        self.mass = random.randrange(14000, 21000)  # *10^26 kilograms
-        self.radius = self.mass * 0.571428571429 + 0.6  # In Solar radii (6.975*10^5km)
-        self.temperature = math.floor(4*math.pi*self.radius*self.radius * 155.424749113 + 3671.88)
+        star.starClass = 'A'
+        star.mass = random.randrange(14000, 21000)  # *10^26 kilograms
+        star.radius = star.mass * 0.571428571429 + 0.6  # In Solar radii (6.975*10^5km)
+        star.temperature = math.floor(4*math.pi*star.radius*star.radius * 155.424749113 + 3671.88)
     elif 0.0073003 < t_val <= 0.0373003:
-        self.starClass = 'F'
-        self.mass = random.randrange(10400, 14000)  # *10^26 kilograms
-        self.radius = self.mass * 0.694444444444 + 1.58333  # In Solar radii (6.975*10^5km)
-        self.temperature = math.floor(4*math.pi*self.radius*self.radius * 187.24110952 + 2888.24)
+        star.starClass = 'F'
+        star.mass = random.randrange(10400, 14000)  # *10^26 kilograms
+        star.radius = star.mass * 0.694444444444 + 1.58333  # In Solar radii (6.975*10^5km)
+        star.temperature = math.floor(4*math.pi*star.radius*star.radius * 187.24110952 + 2888.24)
     elif 0.0373003 < t_val <= 0.1133:
-        self.starClass = 'G'
-        self.mass = random.randrange(8000, 10400)  # *10^26 kilograms
-        self.radius = self.mass * 0.791666666667 + 0.326667  # In Solar radii (6.975*10^5km)
-        self.temperature = math.floor(4*math.pi*self.radius*self.radius * 158.797648383 + 3360.94)
+        star.starClass = 'G'
+        star.mass = random.randrange(8000, 10400)  # *10^26 kilograms
+        star.radius = star.mass * 0.791666666667 + 0.326667  # In Solar radii (6.975*10^5km)
+        star.temperature = math.floor(4*math.pi*star.radius*star.radius * 158.797648383 + 3360.94)
     elif 0.1133 < t_val <= 0.2343:
-        self.starClass = 'K'
-        self.mass = random.randrange(4500, 8000)  # *10^26 kilograms
-        self.radius = self.mass * 1.34615384615 + 0.365714  # In Solar radii (6.975*10^5km)
-        self.temperature = math.floor(4*math.pi*self.radius*self.radius * 276.56674541 + 1997.03)
+        star.starClass = 'K'
+        star.mass = random.randrange(4500, 8000)  # *10^26 kilograms
+        star.radius = star.mass * 1.34615384615 + 0.365714  # In Solar radii (6.975*10^5km)
+        star.temperature = math.floor(4*math.pi*star.radius*star.radius * 276.56674541 + 1997.03)
     else:
-        self.starClass = 'M'
-        self.mass = random.randrange(800, 4500)  # *10^26 kilograms
-        self.radius = self.mass * 1.75675675676 + -0.77973  # In Solar radii (6.975*10^5km)
-        self.temperature = math.floor(4*math.pi*self.radius*self.radius * 212.206590789 + 2393.33)
-
-    self.orbit = '0'
-    self.orbitRadius = 0
+        star.starClass = 'M'
+        star.mass = random.randrange(800, 4500)  # *10^26 kilograms
+        star.radius = star.mass * 1.75675675676 + -0.77973  # In Solar radii (6.975*10^5km)
+        star.temperature = math.floor(4*math.pi*star.radius*star.radius * 212.206590789 + 2393.33)
 
 
 
 
-def generate(self, name, seed, lumen, minOrbit, ismoon=False):
+def planetGen(name, seed, lumen, minOrbit, maxOrbit, ismoon=False):
 
     # how can we estamate the temprature / solar radeation a planet recieves in a solar system?
     # how can we aproxemate atmoshpears?
     # how can we create a distrobution of planets in a semi acurate way
 
-    self.name = name
-    self.seed = seed
-    random.seed = seed
+    planet = Planet(name=name, seed=seed)
 
+    random.seed = planet.seed
 
-    if minimum_orbit == 0 or minimum_orbit < 0 or minimum_orbit is None:
-        minimum_orbit = central_body.radius * 1.05 + 0.5*central_body.radius
-    self.radius = random.randrange(minimum_orbit, central_body.mass * 1000000000000000000000000000)  # In Solar radii (6.975*10^5km)  # Completely Arbitary, will need additional research but probably out of the scope of game
+    planet.axis = random.randint(0, 10)  # Planets inclination relative to the orbital plane
+    planet.tilt = random.randrange(0, 90)  # Planet's axis tilt in degrees from vertical
 
-    t_val = random.random()
-    if self.radius < central_body.radius * 300:
-        if t_val <= 0.5:
-            self.planetType = 'rock'
-        else:
-            self.planetType = 'gas'
-        # Rocky or Gas (0% ice planet)
-    if central_body.radius * 300 <= self.radius < central_body.radius * 700: # Semi habitable? Needs additional research
-        if t_val <= 0.45:
-            self.planetType = 'rock'
-        elif 0.45 < t_val <= 0.8:
-            self.planetType = 'gas'
-        else:
-            self.planetType = 'ice'
-        # Rocky or Gas or ice (45% rocky, 35% gas, 20% ice
+    if ismoone:
+        # idk about moon stuff
+        pass
     else:
-        if t_val <= 0.1:
-            self.type = 'gas'
-        if 0.1 < t_val <= 0.55:
-            self.type = 'rock'
+        t_val = random.random()
+
+        if t_val <= 0.3:
+            planet.type = 'gas'
+            planet.mass = random.range(5.685*(10**6), 1.899*(10**7)) # i just put in the mass of jupeter and satern as endpoints Units: 10^20 kg
+        if 0.3 < t_val <= 0.7:
+            planet.type = 'rock'
+            planet.mass = random.range(3.301*(10**3), 5.972*(10**4)) # Units: 10^20 kg
         else:
-            self.type = 'ice'
-        # Rocky or ice (~10% of gas, 45% other)
-    self.axis = random.randint(0, 10)  # Planets inclination relative to the orbital plane
-    self.tilt = random.randrange(0, 90)  # Planet's axis tilt in degrees from vertical
-    self.planetRadius = math.floor(random.randrange(900, 0.05 * central_body.radius))  # The base radius of the planet itself in km
-    if self.planetType is 'gas':
-        # gassy things
-        self.mass = math.floor((4/3) * math.pi * self.planetRadius**3 * random.randrange(0.9, 1.8))  # in kg
-        self.atmos = None
-        self.temperature = random.randint(800, 1400)  # Surface temperature  in kelvin
-    if self.planetType is 'rock':
-        # rocky things
-        self.mass = math.floor((4/3) * math.pi * self.planetRadius**3 * random.randrange(0.8, 1.1))
-        self.atmos = random.randint(0, 1)
-        if self.atmos == 1:
-            self.temperature = random.randint(230, 380)  # Surface temperature
-        else:
-            self.temperature = random.randint(125, 800)
-    else:
-        # icy things
-        self.mass = math.floor((4/3) * math.pi * self.planetRadius**3 * random.randrange(0.9, 1.1))
-        self.atmos = random.randint(0, 1)
-        if self.atmos == 1:
-            self.temperature = random.randint(200, 270)
-        else:
-            self.temperature = random.randint(125, 270)  # Surface temperature
-    self.gravity = ((6.67*10**-11) * self.mass)/self.planetRadius**2  # Gravitational pull at planetRadius
+            planet.type = 'ice'
+            planet.mass = random.range(8.682*(10**5), 1.024*(10**6)) # Units: 10^20 kg
 
 
-    for i in random.randint(0,3):
-        self.generate()
+        planet.gravity = ((6.67*10**-11) * planet.mass)/planet.planetRadius**2  # Gravitational pull at planetRadius
--- mysite/solapi/models.py
+++ mysite/solapi/models.py
@@ -6,154 +6,64 @@ import random
 import math
 # Create your models here.
 
+class Atmosphere(models.Model):
+
+    presure = models.FloatField() # presure of the atmosphere Units: KPa
+
+    # all the rest are in percentages
+    nitrogen = models.FloatField()
+    oxygen = models.FloatField()
+    argon = models.FloatField()
+    carbonDioxide = models.FloatField()
+    neon = models.FloatField()
+    helium = models.FloatField()
+    methane = models.FloatField()
+    sulpherDioxide = models.FloatField()
+    hydrogen = models.FloatField()
+    sodium = models.FloatField()
+    potasium = models.FloatField()
+
+class Orbit(models.Model):
+    # orbit shit
+    # https://www.desmos.com/calculator/xr6lrvad5a
+
+    a = models.FloatField() # semi major axis of the elipse Units: AU
+    b = models.FloatField() # semi minor axis of the elipse Units: AU
+    rotation = models.FloatField() # the rotation of the orbit around the center Units: deg
+    bigM = models.FloatField() # the mass of the larger object Units: KG
+
 class Star(models.Model):
     name = models.CharField(max_length=100)
+    seed = models.CharField(max_length=100)
     starClass = models.CharField(max_length=100)
     mass = models.IntegerField()
     radius = models.IntegerField()
     temperature = models.IntegerField()
-    orbit = models.CharField(max_length=100)
-    orbitRadius = models.IntegerField()
-    seed = models.IntegerField()
+
+    orbit = models.OneToOneField(Orbit, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
-    def generate(self, name, seed):
-        self.name = name
-        self.seed = seed
-        random.seed = seed
-
-        t_val = random.random()  # Temp variable
-
-        if t_val <= 0.0000003:
-            self.starClass = 'O'
-            self.mass = random.randrange(160000, 320000)  # *10^26 kilograms
-            self.radius = self.mass * 0.4125  # In Solar radii (6.975*10^5km)
-            self.temperature = math.floor(4*math.pi*self.radius*self.radius * 42.6264386916 + 6666.67)  # Kelvin (many thousands) The area of the sun multiplies by the gradient of the relationship between area and termperate for its class
-        elif 0.0000003 < t_val <= 0.0013003:
-            self.starClass = 'B'
-            self.mass = random.randrange(21000, 160000)  # *10^26 kilograms
-            self.radius = self.mass * 0.345323741007 + 1.47842  # In Solar radii (6.975*10^5km)
-            self.temperature = math.floor(4*math.pi*self.radius*self.radius * 39.4729521557 + 8392.86)
-        elif 0.0013003 < t_val <= 0.0073003:
-            self.starClass = 'A'
-            self.mass = random.randrange(14000, 21000)  # *10^26 kilograms
-            self.radius = self.mass * 0.571428571429 + 0.6  # In Solar radii (6.975*10^5km)
-            self.temperature = math.floor(4*math.pi*self.radius*self.radius * 155.424749113 + 3671.88)
-        elif 0.0073003 < t_val <= 0.0373003:
-            self.starClass = 'F'
-            self.mass = random.randrange(10400, 14000)  # *10^26 kilograms
-            self.radius = self.mass * 0.694444444444 + 1.58333  # In Solar radii (6.975*10^5km)
-            self.temperature = math.floor(4*math.pi*self.radius*self.radius * 187.24110952 + 2888.24)
-        elif 0.0373003 < t_val <= 0.1133:
-            self.starClass = 'G'
-            self.mass = random.randrange(8000, 10400)  # *10^26 kilograms
-            self.radius = self.mass * 0.791666666667 + 0.326667  # In Solar radii (6.975*10^5km)
-            self.temperature = math.floor(4*math.pi*self.radius*self.radius * 158.797648383 + 3360.94)
-        elif 0.1133 < t_val <= 0.2343:
-            self.starClass = 'K'
-            self.mass = random.randrange(4500, 8000)  # *10^26 kilograms
-            self.radius = self.mass * 1.34615384615 + 0.365714  # In Solar radii (6.975*10^5km)
-            self.temperature = math.floor(4*math.pi*self.radius*self.radius * 276.56674541 + 1997.03)
-        else:
-            self.starClass = 'M'
-            self.mass = random.randrange(800, 4500)  # *10^26 kilograms
-            self.radius = self.mass * 1.75675675676 + -0.77973  # In Solar radii (6.975*10^5km)
-            self.temperature = math.floor(4*math.pi*self.radius*self.radius * 212.206590789 + 2393.33)
-
-        self.orbit = '0'
-        self.orbitRadius = 0
 
 
 class Planet(models.Model):
     name = models.CharField(max_length=100)
+    seed = models.CharField(max_length=100)
     planetType = models.CharField(max_length=100)
     mass = models.IntegerField()
     radius = models.IntegerField()
-    temperature = models.IntegerField()
-    atmos = models.IntegerField()
-    seed = models.IntegerField()
     gravity = models.IntegerField()
     tilt = models.IntegerField()
     axis = models.IntegerField()
-    planetRadius = models.IntegerField()
-    orbit = models.CharField(max_length=100)
-    orbitRadius = models.IntegerField()
 
+    atmosphere = models.OneToOneField(Atmosphere, on_delete=models.CASCADE)
+    orbit = models.OneToOneField(Orbit, on_delete=models.CASCADE)
 
     moons = models.ManyToManyField('self')
 
     def __str__(self):
         return self.name
-
-    def generate(self, name, seed, lumen, minOrbit, ismoon=False):
-
-        # how can we estamate the temprature / solar radeation a planet recieves in a solar system?
-        # how can we aproxemate atmoshpears?
-        # how can we create a distrobution of planets in a semi acurate way
-
-        self.name = name
-        self.seed = seed
-        random.seed = seed
-
-
-        if minimum_orbit == 0 or minimum_orbit < 0 or minimum_orbit is None:
-            minimum_orbit = central_body.radius * 1.05 + 0.5*central_body.radius
-        self.radius = random.randrange(minimum_orbit, central_body.mass * 1000000000000000000000000000)  # In Solar radii (6.975*10^5km)  # Completely Arbitary, will need additional research but probably out of the scope of game
-
-        t_val = random.random()
-        if self.radius < central_body.radius * 300:
-            if t_val <= 0.5:
-                self.planetType = 'rock'
-            else:
-                self.planetType = 'gas'
-            # Rocky or Gas (0% ice planet)
-        if central_body.radius * 300 <= self.radius < central_body.radius * 700: # Semi habitable? Needs additional research
-            if t_val <= 0.45:
-                self.planetType = 'rock'
-            elif 0.45 < t_val <= 0.8:
-                self.planetType = 'gas'
-            else:
-                self.planetType = 'ice'
-            # Rocky or Gas or ice (45% rocky, 35% gas, 20% ice
-        else:
-            if t_val <= 0.1:
-                self.type = 'gas'
-            if 0.1 < t_val <= 0.55:
-                self.type = 'rock'
-            else:
-                self.type = 'ice'
-            # Rocky or ice (~10% of gas, 45% other)
-        self.axis = random.randint(0, 10)  # Planets inclination relative to the orbital plane
-        self.tilt = random.randrange(0, 90)  # Planet's axis tilt in degrees from vertical
-        self.planetRadius = math.floor(random.randrange(900, 0.05 * central_body.radius))  # The base radius of the planet itself in km
-        if self.planetType is 'gas':
-            # gassy things
-            self.mass = math.floor((4/3) * math.pi * self.planetRadius**3 * random.randrange(0.9, 1.8))  # in kg
-            self.atmos = None
-            self.temperature = random.randint(800, 1400)  # Surface temperature  in kelvin
-        if self.planetType is 'rock':
-            # rocky things
-            self.mass = math.floor((4/3) * math.pi * self.planetRadius**3 * random.randrange(0.8, 1.1))
-            self.atmos = random.randint(0, 1)
-            if self.atmos == 1:
-                self.temperature = random.randint(230, 380)  # Surface temperature
-            else:
-                self.temperature = random.randint(125, 800)
-        else:
-            # icy things
-            self.mass = math.floor((4/3) * math.pi * self.planetRadius**3 * random.randrange(0.9, 1.1))
-            self.atmos = random.randint(0, 1)
-            if self.atmos == 1:
-                self.temperature = random.randint(200, 270)
-            else:
-                self.temperature = random.randint(125, 270)  # Surface temperature
-        self.gravity = ((6.67*10**-11) * self.mass)/self.planetRadius**2  # Gravitational pull at planetRadius
-
-
-        for i in random.randint(0,3):
-            self.generate()
 
 
 
@@ -163,6 +73,8 @@ class System(models.Model):
     stars = models.ManyToManyField(Star)
     planets = models.ManyToManyField(Planet)
     #ast = models.ManyToManyField(Ast)
+
+    orbit = models.OneToOneField(Orbit, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
